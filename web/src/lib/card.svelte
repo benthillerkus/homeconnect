b6f0ce8df638fca6746c75d166078305d5647cb1
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-11-24 22:28:28 +0100

    - add component skeletons - implement like component

---
+++ web/src/lib/card.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+  import Heart from "$lib/heart.svelte"
+</script>
+
+<div>
+  <canvas></canvas>
+  <Heart></Heart>
+</div>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  import Card from "$lib/card.svelte"
+</script>
+
+<h1>Homeconnect</h1>
+<Card></Card>
