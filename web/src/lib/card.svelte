e52f98bb4380e57eaca4edcb465cfa43a2596e2b
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-11-25 15:07:05 +0100

    add card css

--- web/src/lib/card.svelte
+++ web/src/lib/card.svelte
@@ -7,15 +7,36 @@
 
 <section>
   <canvas />
-  <Heart />
+  <div id="actions">
+    <Heart />
+  </div>
 </section>
 
 <style>
   section {
     background-color: aliceblue;
+    display: grid;
+    grid: "image image" 80% "info actions" auto / 50% auto;
+    gap: 1rem;
     border-radius: 18px;
-    width: 500px;
-    height: 550px;
-    aspect-ratio: 0.8;
+    width: 460px;
+    height: 510px;
+    padding: 20px;
+  }
+
+  canvas {
+    grid-area: image;
+    width: 100%;
+    height: 100%;
+    border-radius: 14px;
+    background-color: beige;
+  }
+
+  #actions {
+    grid-area: actions;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
   }
 </style>
