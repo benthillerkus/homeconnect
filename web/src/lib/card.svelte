f9b9afaa24ec61518ffe8afd7d61b235228af5aa
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-11-25 03:17:33 +0100

    - add skeleton css - make html semantic - break like component

--- web/src/lib/card.svelte
+++ web/src/lib/card.svelte
@@ -1,8 +1,21 @@
 <script context="module">
-  import Heart from "$lib/heart.svelte"
 </script>
 
-<div>
-  <canvas></canvas>
-  <Heart></Heart>
-</div>
+<script>
+  import Heart from "$lib/heart.svelte";
+</script>
+
+<section>
+  <canvas />
+  <Heart />
+</section>
+
+<style>
+  section {
+    background-color: aliceblue;
+    border-radius: 18px;
+    width: 500px;
+    height: 550px;
+    aspect-ratio: 0.8;
+  }
+</style>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,6 +1,64 @@
 <script context="module">
-  import Card from "$lib/card.svelte"
 </script>
 
-<h1>Homeconnect</h1>
-<Card></Card>
+<script>
+  import Card from "$lib/card.svelte";
+</script>
+
+<section id="page">
+  <header>
+    <h1>Homeconnect</h1>
+  </header>
+  <main>
+    <Card />
+  </main>
+  <footer>
+    <small>
+      <nav>
+        <a>about</a>
+        <a href="https://github.com/benthillerkus/homeconnect">source</a>
+        <a>licenses</a>
+      </nav>
+    </small>
+  </footer>
+</section>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    color-scheme: light;
+  }
+
+  #page {
+    display: grid;
+    height: 100vh;
+    width: 100vw;
+    grid-template: "header" 10% "main" auto "footer" 30px;
+  }
+  header {
+    grid-row: header;
+    text-align: center;
+  }
+
+  main {
+    grid-row: main;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  footer {
+    grid-row: footer;
+    display: flex;
+    justify-content: center;
+  }
+
+  footer small nav a {
+    padding: 0.2rem;
+    opacity: 0.5;
+    text-decoration: none;
+    color: black;
+  }
+</style>
