a58a7f86ed697bf9052c56e802232291fe83e22f
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-11-26 00:41:19 +0100

    log secrets

--- web/src/lib/_db.js
+++ web/src/lib/_db.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(import.meta.env.NEXT_PUBLIC_SUPABASE_URL, import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
 export const supabase = createClient(
   import.meta.env.NEXT_PUBLIC_SUPABASE_URL,
   import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
