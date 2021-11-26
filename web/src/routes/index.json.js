aad3ae4352a2d7597d823001fe3ca51567a7aa00
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-11-26 01:47:04 +0100

    have *some* api request so that something is visible rn

--- web/src/routes/index.json.js
+++ web/src/routes/index.json.js
@@ -1,15 +1,16 @@
 import { supabase } from "$lib/_db.js"
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get({ params }) {
-  const res = await supabase.storage.from("images").list("", {
-    sortBy: { column: "created_at", order: "desc" }
-  })
-
-  console.log(res)
+export async function get( { params } ) {
+  const res = await supabase.storage.listBuckets()
+  // const res = await supabase.storage.from("images").list()
+  // const res = await supabase.storage.from( "images" ).list( "", {
+  //   sortBy: { column: "created_at", order: "desc" }
+  // } )
+  console.log( res )
 
   return {
     status: res.status,
-    body: JSON.stringify(res)
+    body: JSON.stringify( res )
   }
 }
