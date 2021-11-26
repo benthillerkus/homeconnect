12ad35ba4633334d83852eefe1f70e971d1a1bb0
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-11-26 02:39:32 +0100

    enable file uploading

--- web/src/hooks.js
+++ web/src/hooks.js
@@ -1,8 +1,31 @@
+import { supabase } from "$lib/_db.js"
+
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ request, resolve }) {
   if (request.method === "POST") {
-    return {
-      status: 202
+    if (request.headers.authorization === `Bearer ${import.meta.env.VITE_HOMECONNECT_ALLOW_POST}`) {
+      const { data, error } = await supabase.storage
+        .from("images")
+        .upload(request.headers.name, request.rawBody, {
+          contentType: request.headers["content-type"]
+        })
+
+      if (error) {
+        console.error(error)
+        return {
+          status: 500
+        }
+      }
+
+      return {
+        status: 200,
+        body: JSON.stringify(data)
+      }
+    } else {
+      return {
+        status: 401,
+        body: "Invalid Authorization Token"
+      }
     }
   }
 
--- web/src/routes/index.json.js
+++ web/src/routes/index.json.js
@@ -1,16 +1,16 @@
 import { supabase } from "$lib/_db.js"
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get( { params } ) {
+export async function get({ params }) {
   const res = await supabase.storage.listBuckets()
   // const res = await supabase.storage.from("images").list()
   // const res = await supabase.storage.from( "images" ).list( "", {
   //   sortBy: { column: "created_at", order: "desc" }
   // } )
-  console.log( res )
+  console.log(res)
 
   return {
     status: res.status,
-    body: JSON.stringify( res )
+    body: JSON.stringify(res)
   }
 }
