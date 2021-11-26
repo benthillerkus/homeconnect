64054d5ef4b1dc115d8ed29a647375a62a8abe38
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-11-27 00:56:16 +0100

    add blinking demo code

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -3,10 +3,12 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   printf("Hello World!\n");
+  digitalWrite(4, digitalRead(4) == LOW ? HIGH : LOW);
   sleep(5);
 }
