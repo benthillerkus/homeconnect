e47e172b882030edbe11f8e9d41297c306c8028c
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-12-02 22:00:08 +0100

    change vibration pattern

--- web/src/lib/card.svelte
+++ web/src/lib/card.svelte
@@ -102,7 +102,7 @@
       const now = Date.now()
       if (now - lastTouch < 500) {
         event.preventDefault()
-        navigator.vibrate([50, 50, 50])
+        navigator.vibrate([75, 25, 50])
         liked = !liked
       } else {
         lastTouch = now
